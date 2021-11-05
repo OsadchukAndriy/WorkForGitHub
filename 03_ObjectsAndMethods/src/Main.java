aa1a47ef203377b37748baf7c1a32109301a40d3
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-05 21:42:19 +0200

    с Integer на int

--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Basket basket = new Basket();
-        basket.add("Milk", 40,10,49);
+        basket.add("Milk", 40,10,56);
         basket.print("Milk");
     }
 }
