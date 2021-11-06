6dbef868f2c86c275dc642ee0be60068b3b31bb9
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-06 12:12:55 +0200

    алфавит

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -4,6 +4,13 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        for(int i = 'А'; i <= 'я'; i++){
+            System.out.println("У буквы - " + (char)i + " код - " + i);
+        }
+        System.out.println("У буквы - " + 'Ë' + " код - " + (int)'Ë');
+        System.out.println("У буквы - " + 'e' + " код - " + (int)'e');
+
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
