a7e2d4b40d0324e12fb2121023ce200f00fc6049
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-07 13:08:03 +0200

    не знаю где ошибка

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,28 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+       double a = Math.PI * (Math.pow (radius, 2));
+        return a;
     }
-
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        double v = 3.0 / 4 * (Math.PI * Math.pow(radius, 3));
+        return v;
     }
-
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
-    }
+        if ((a+b)>c && (a+c)>b && (b+c)>a)
+        {
+            return true;
+        }
+            return false;
+        }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+       double p = (a + b + c) / 2;
+       double s = p * (p - a) * (p - b) * (p - c);
+       double ss = Math.pow(s, 2);
+        return ss;
     }
 }
