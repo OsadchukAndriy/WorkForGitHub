deb4068f4f83e4a688d0d3d1f2d1e3a84db789be
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-08 16:38:37 +0200

    все исправил

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -6,7 +6,7 @@ public class GeometryCalculator {
     }
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        double v = 4 * (Math.PI * Math.pow(radius, 3)) / 3;
+        double v = 4 * Math.PI * Math.pow(Math.abs(radius), 3)/3.0;
         return Math.abs(v);
     }
     public static boolean isTrianglePossible(double a, double b, double c) {
@@ -20,9 +20,12 @@ public class GeometryCalculator {
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
+
+        if (!isTrianglePossible(a, b, c)) {
+            return -1.0;
+        }
         double p = (a + b + c) / 2;
-        double s = p * (p - a) * (p - b) * (p - c);
-        double ss = Math.sqrt(s);
-        return Math.abs(ss);
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
     }
 }
