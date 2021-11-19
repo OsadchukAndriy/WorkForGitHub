ac207ee81d9a7b241f81a156f3622cdb7bb2f56c
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-19 16:05:01 +0200

    не могу пройти первый тест

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
+
       int spaceCount = 0;
       int firstSpace = input.indexOf(' ');
       int lastSpace = input.lastIndexOf(' ');
@@ -18,13 +19,27 @@ public class Main {
       String lastName = input.substring(lastSpace + 1);
       boolean printName = false;
 
-      System.out.println("Фамилия: " + firstName);
-      System.out.println("Имя: " + secondName);
-      System.out.println("Отчество: " + lastName);
+      for(int a = 0; a < input.length(); a++){
+        char symbol = input.charAt(a);
+        if (symbol == ' '){
+          spaceCount++;
+        }
+        if ((symbol == 32 || symbol == 45 || (symbol > 1048 && symbol < 1104)) && spaceCount == 2){
+          printName = true;
+        } else {
+          printName = false;
+        }
+
+      }
+      if (printName){
+        System.out.println("Фамилия: " + secondName + "\nИмя: " + firstName + "\nОтчество: " + lastName );
+      }
+      else {
+        System.out.println("Введенная строка не является ФИО");
+      }
 
     }
   }
-
 }
 
 
