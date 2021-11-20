174290a78d7eef6bc7d22d4ef0348b944c14c83f
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-20 11:59:53 +0200

    прошол все тесты с регулярными выражениями

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,26 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей");
   }
 
   public static int calculateSalarySum(String text){
+
+    Pattern pattern = Pattern.compile("\\d+");
+    Matcher matcher = pattern.matcher(text);
+    int sum = 0;
+    while(matcher.find()){
+      int start = matcher.start();
+      int end = matcher.end();
+      String result = text.substring(start, end);
+      int number = Integer.parseInt(result);
+      sum = sum + number;
+    }
     //TODO: реализуйте метод
-    return -1;
+    return sum;
   }
 
 }
