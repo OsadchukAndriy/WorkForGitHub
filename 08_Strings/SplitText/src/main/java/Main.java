adea82c74add3558a3e858d5dfc1e8c3f6dc0934
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-20 14:17:39 +0200

    Split доделал через if

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,12 +1,20 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    splitTextIntoWords("Корректность работы программы независимо от передаваемого ей на вход текста");
   }
 
   public static String splitTextIntoWords(String text) {
     //TODO реализуйте метод
-    return "";
-  }
-
-}
+    String string = "";
+    StringBuilder buil = new StringBuilder();
+    String[] words = text.split("[\\s |\\,|\\.|;|'|\\-|[0-9]\\s]");
+    for(int i = 0; i < words.length; i++){
+      if (words[i] == ""){
+        words[i].split("[\\s]");
+      } else {
+        buil.append(words[i].trim() + System.lineSeparator());
+      }}
+    string = "" + buil;
+    return string.trim();
+  }}
