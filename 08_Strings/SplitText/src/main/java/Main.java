ef5e66e1c1b30355ba2676f21dbd578c703df9bc
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-23 12:45:43 +0200

    Split доделал через if и украсил код

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,20 +1,22 @@
 public class Main {
 
-  public static void main(String[] args) {
-    splitTextIntoWords("Корректность работы программы независимо от передаваемого ей на вход текста");
-  }
+    public static void main(String[] args) {
+        splitTextIntoWords("Корректность работы программы независимо от передаваемого ей на вход текста");
+    }
 
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    String string = "";
-    StringBuilder buil = new StringBuilder();
-    String[] words = text.split("[\\s |\\,|\\.|;|'|\\-|[0-9]\\s]");
-    for(int i = 0; i < words.length; i++){
-      if (words[i] == ""){
-        words[i].split("[\\s]");
-      } else {
-        buil.append(words[i].trim() + System.lineSeparator());
-      }}
-    string = "" + buil;
-    return string.trim();
-  }}
+    public static String splitTextIntoWords(String text) {
+        //TODO реализуйте метод
+        String string = "";
+        StringBuilder buil = new StringBuilder();
+        String[] words = text.split("[\\s |\\,|\\.|;|'|\\-|[0-9]\\s]");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] == "") {
+                words[i].split("[\\s]");
+            } else {
+                buil.append(words[i].trim() + System.lineSeparator());
+            }
+        }
+        string = "" + buil;
+        return string.trim();
+    }
+}
