802ea9252e37fd4288f264e1ed86ecf9970a8f61
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-24 23:18:57 +0200

    доделал roges красиво

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -4,27 +4,27 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
 
-      final String regex = "([А-Я][а-я]+-?[А-Я]?[а-я]+)" + "\\s" + "([А-Я][а-я]+)" + "\\s" + "([А-Я][а-я]+)";
-      final Pattern pattern = Pattern.compile(regex);
-      Matcher matcher = pattern.matcher(input);
-      if (!matcher.matches()) {
-        System.out.println("Введенная строка не является ФИО");
-        break;
-      }
+            final String regex = "([А-Я][а-я]+-?[А-Я]?[а-я]+)" + "\\s" + "([А-Я][а-я]+)" + "\\s" + "([А-Я][а-я]+)";
+            final Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+            if (!matcher.matches()) {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
 
-      System.out.println("Фамилия: " + matcher.group(1));
-      System.out.println("Имя: " + matcher.group(2));
-      System.out.println("Отчество: " + matcher.group(3));
+            System.out.println("Фамилия: " + matcher.group(1));
+            System.out.println("Имя: " + matcher.group(2));
+            System.out.println("Отчество: " + matcher.group(3));
+        }
+
     }
-
-  }
-    }
+}
