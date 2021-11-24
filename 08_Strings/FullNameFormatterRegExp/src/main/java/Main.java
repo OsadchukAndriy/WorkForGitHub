4ac7823370c1ecde6e49ac2b231ea6435f1ad322
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-24 23:16:05 +0200

    доделал roges

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,9 +12,19 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-    }
-  }
 
-}
+      final String regex = "([А-Я][а-я]+-?[А-Я]?[а-я]+)" + "\\s" + "([А-Я][а-я]+)" + "\\s" + "([А-Я][а-я]+)";
+      final Pattern pattern = Pattern.compile(regex);
+      Matcher matcher = pattern.matcher(input);
+      if (!matcher.matches()) {
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
+
+      System.out.println("Фамилия: " + matcher.group(1));
+      System.out.println("Имя: " + matcher.group(2));
+      System.out.println("Отчество: " + matcher.group(3));
+    }
+
+  }
+    }
