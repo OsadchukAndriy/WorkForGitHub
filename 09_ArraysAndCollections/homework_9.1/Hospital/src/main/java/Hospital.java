25de12f3119c89cc87a5adda652f99d4e2bf087e
andrejosadcuk <Rudyst.1995@gmail.com>
2021-11-29 11:41:25 +0200

    Hospital без магических чисел

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -2,15 +2,17 @@ import java.util.Locale;
 import java.util.Random;
 
 public class Hospital {
+    public static final int MIN_TEMPERATURE = 32;
+    public static final int MAX_TEMPERATURE = 40;
+    public static final float MIN_HEALTHY_TEMPERATURE = 36.2f;
+    public static final float MAX_HEALTHY_TEMPERATURE = 37.0f;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         float[] patientsTemperatures = new float[patientsCount];
         for (int i = 0; i < patientsTemperatures.length; i++) {
             float rounding = (float) Math.pow(10, 1);
-            float min = 32;
-            float max = 40;
-            float randomTemp = (float) ((Math.random() * (max - min)) + min);
+            float randomTemp = (float) ((Math.random() * (MAX_TEMPERATURE - MIN_TEMPERATURE)) + MIN_TEMPERATURE);
             float randomTemperatures = Math.round(randomTemp * rounding) / rounding;
             patientsTemperatures[i] = randomTemperatures;
         }
@@ -24,7 +26,7 @@ public class Hospital {
         for (int i = 0; i <= temperatureData.length - 1; i++) {
             patientsTemperatures += temperatureData[i] + " ";
             temperaturesSum += temperatureData[i];
-            if (temperatureData[i] >= 36.2 && temperatureData[i] <= 37.0)
+            if (temperatureData[i] >= MIN_HEALTHY_TEMPERATURE && temperatureData[i] <= MAX_HEALTHY_TEMPERATURE)
                 healthyPatientsCount++;
         }
 
