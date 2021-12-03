b240aff84a046782473b8ffcd0313423469c553a
andrejosadcuk <Rudyst.1995@gmail.com>
2021-12-03 11:17:57 +0200

    прошол тесты  EmailList

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,25 @@
-import java.util.Collections;
-import java.util.List;
+import org.apache.commons.collections.list.TreeList;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
+    private final TreeSet<String> treeSet = new TreeSet<>();
 
     public void add(String email) {
-        // TODO: валидный формат email добавляется
+        Pattern p = Pattern.compile("([A-z]+)@([A-z]+)\\.([a-z]+)");
+        Matcher m = p.matcher(email.toLowerCase(Locale.ROOT));
+        if (!m.matches() || treeSet.contains(email.toLowerCase(Locale.ROOT))) {
+            System.out.println("Неверный формат email");
+        } else {
+            treeSet.add(email);
+            System.out.println("Добавлен email" + email);
+        }
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new ArrayList<>(treeSet);
     }
 
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,33 +1,38 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+        EmailList emailList = new EmailList();
+
         while (true) {
-            String input = scanner.nextLine();
+            String input = scan.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+            Pattern p = Pattern.compile("(ADD|LIST)\\\\s?(.*)");
+            Matcher m = p.matcher(input);
+            String emailAdress = m.group(2).toLowerCase();
+            String command = m.group(1);
+
+            if (!m.matches()) {
+                System.out.println(WRONG_EMAIL_ANSWER);
+                break;
+            }
+            switch (command) {
+                case "LIST":
+                    emailList.getSortedEmails();
+                case "ADD":
+                    emailList.add(emailAdress);
+                    break;
+
+            }
         }
     }
 }
