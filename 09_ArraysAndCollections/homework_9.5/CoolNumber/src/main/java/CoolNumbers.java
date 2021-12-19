c562a0f24e62a2832f528e9d450f98b10362477a
andrejosadcuk <Rudyst.1995@gmail.com>
2021-12-19 15:04:06 +0200

    прошол все тесты и прописал main

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,28 +1,100 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        ArrayList<String> coolNumbersList = new ArrayList<>();
+        ArrayList<String> coolNumbersListWithoutRegion = new ArrayList<>();
+
+        String numberplate = "";
+        String numberplateWithoutRegion = "";
+
+        char x; // первая буква
+        char z; // вторая буква
+        char y; // третья буква
+
+        ArrayList<Character> allowedLetters = new ArrayList<>() {{
+            add('А');
+            add('В');
+            add('Е');
+            add('К');
+            add('М');
+            add('Н');
+            add('О');
+            add('Р');
+            add('С');
+            add('Т');
+            add('У');
+            add('Х');
+        }};
+
+        for (Character firstLetter : allowedLetters) {
+            x = firstLetter;
+            for (int n = 0; n < 10; n++) {
+                for (Character secondLetter : allowedLetters) {
+                    z = secondLetter;
+                    for (Character thirdLetter : allowedLetters) {
+                        y = thirdLetter;
+                        numberplateWithoutRegion = String.format("%c%d%d%d%c%c", x, n, n, n, z, y);
+                        coolNumbersListWithoutRegion.add(numberplateWithoutRegion);
+                    }
+                }
+            }
+        }
+
+        for (String numberWithoutRegion : coolNumbersListWithoutRegion) {
+            for (int j = 0; j <= 199; j++) {
+                if (j < 10) {
+                    numberplate = String.format("%s0%d", numberWithoutRegion, j);
+                    coolNumbersList.add(numberplate);
+                } else {
+                    numberplate = String.format("%s%d", numberWithoutRegion, j);
+                    coolNumbersList.add(numberplate);
+                }
+            }
+        }
+
+        return coolNumbersList;
     }
 
+
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+
+        for (String brut : list)
+            if (brut.contains(number)) {
+                return true;
+            } else {
+                return false;
+            }
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
+        Collections.sort(sortedList);
+        for (String brut : sortedList) {
+            Collections.binarySearch(sortedList, number);
+            if (brut.contains(number)) {
+                return true;
+            }
+        }
         return false;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
+        for (String brut : hashSet)
+            if (brut.contains(number)) {
+                return true;
+            }
         return false;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
+        for (String brut : treeSet)
+            if (brut.contains(number)) {
+                return true;
+            }
         return false;
     }
 
--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
     /*
     TODO:
@@ -6,7 +11,52 @@ public class Main {
      - проанализоровать полученные данные
      */
 
-    public static void main(String[] args) {
+    public static final String TRUE = "найден";
+    public static final String WRONG = "не найден";
 
+    public static void main(String[] args) {
+        CoolNumbers coolNumbers = new CoolNumbers();
+        String number = new String();
+        List<String> list = new ArrayList<>();
+        HashSet<String> hash = new HashSet<>();
+        TreeSet<String> tree = new TreeSet<>();
+
+        long start = System.nanoTime();
+        coolNumbers.bruteForceSearchInList(list, number);
+        long g = (System.nanoTime() - start);
+        if (true) {
+            System.out.println("Поиск перебором: " + TRUE + " поиск занял " + g);
+        } else {
+            System.out.println("Поиск перебором: " + WRONG + " поиск занял " + g);
+        }
+        long start1 = System.nanoTime();
+        coolNumbers.binarySearchInList(list, number);
+        long d = (System.nanoTime() - start1);
+        if (true) {
+            System.out.println("Поиск перебором: " + TRUE + " поиск занял " + d);
+        } else {
+            System.out.println("Поиск перебором: " + WRONG + " поиск занял " + d);
+        }
+        long start2 = System.nanoTime();
+        coolNumbers.searchInHashSet(hash, number);
+        long q = (System.nanoTime() - start2);
+        if (true) {
+            System.out.println("Поиск перебором: " + TRUE + " поиск занял " + q);
+        } else {
+            System.out.println("Поиск перебором: " + WRONG + " поиск занял " + q);
+        }
+        long start3 = System.nanoTime();
+        coolNumbers.searchInTreeSet(tree, number);
+        long j = (System.nanoTime() - start3);
+        if (true) {
+            System.out.println("Поиск перебором: " + TRUE + " поиск занял " + j);
+        } else {
+            System.out.println("Поиск перебором: " + WRONG + " поиск занял " + j);
+        }
     }
+
+
 }
+
+
+
