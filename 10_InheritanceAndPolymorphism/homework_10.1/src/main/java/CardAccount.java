f72e88fa7e48f5476a3e1c6d5f74fa32b97fe185
andrejosadcuk <Rudyst.1995@gmail.com>
2021-12-25 22:07:11 +0200

    сделал все без мейна1

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/CardAccount.java
@@ -1,9 +1,9 @@
 public class CardAccount extends BankAccount {
-    public static final double commissionPercent = 1;
+
 
     @Override
     public boolean take(double amountToTake) {
-        return super.take(amountToTake + amountToTake * (commissionPercent / 100));
+        return super.take(amountToTake + amountToTake * (1 / 100));
     }
 
 }
